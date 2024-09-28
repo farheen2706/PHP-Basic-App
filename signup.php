41982450eb55d35eea9d4408ad4e07c7516dda4e
farheen2706 <54413405+farheen2706@users.noreply.github.com>
2024-09-28 11:05:51 +0000

    Update signup.php

--- signup.php
+++ signup.php
@@ -11,7 +11,7 @@
         <h2>Signup</h2>
         <form action="signup_process.php" method="POST">
             <input type="text" name="username" placeholder="Username" required>
-            <input type="email" name="email" placeholder="Email" required>
+            <input type="email" name="email" placeholder="E-mail" required>
             <input type="password" name="password" placeholder="Password" required>
             <button type="submit">Signup</button>
         </form>
